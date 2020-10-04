93495e55c209ded0788b90e4d8a099a947d3878e
bhaskar4u <bhaskarsahni121@gmail.com>
2020-10-05 04:45:32 +0530

    essential module required

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("first test");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const Cors = require('cors')
+
+dotenv.config({ path: 'config/config.env' })
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome To CityLink")
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(`Server Not started${err}`);
+    }
+    console.log(`Server Running in ${process.env.NODE_ENV} on PORT :${PORT}`);
+})
